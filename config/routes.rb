a28ebd6e935f2854e3b4ef8eb05b557191f69b40
Kristen Hazard <kristen@hazardbio.com>
2014-08-08 11:17:56 -0700

    ember in engine working without templates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  #mount Emberengine::Engine, at: "emberengine"
+  mount Emberengine::Engine, at: "emberengine"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
